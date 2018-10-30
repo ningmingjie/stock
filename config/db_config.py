26a73a1e698f3e59fb763ea5dad1b9f2c69a0559
宁铭杰 <bream_n@163.com>
2018-10-30 11:20:05 +0800

    暂存

--- config/db_config.py
+++ config/db_config.py
@@ -71,8 +71,8 @@ class DB(object):
     # 新增记录
     def insertData(self, sql):
         res = True
-        #print self._conn
         if (self._conn):
+            print  self._conn
             try:
                 self._cursor.execute(sql)
             except Exception, data:
