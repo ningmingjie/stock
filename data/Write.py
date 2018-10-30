3a59f3d5ec9759f5ace94b89cf48e4dbdc513fd6
宁铭杰 <bream_n@163.com>
2018-10-30 15:02:49 +0800

    暂存

--- data/Write.py
+++ data/Write.py
@@ -6,18 +6,21 @@ import pandas as pd
 
 class Write:
 
+    def __init__(self):
+        self._path = "/data/share/loudou/stock/"
+
     def getTodayAll(self):
         data = ts.get_today_all()
         data.reset_index(inplace=True)
         dt = np.array(data)
         dts = dt.tolist()
         for st in dts:
-            self.write(st[1])
+            self.write(st[1], st[2])
         return True
 
-    def write(self, ticker):
-        fo = open("stockTicker.txt", "a")
-        fo.write(ticker + "\n")
+    def write(self, secCode, secName):
+        fo = open(self._path + "stockTicker.txt", "a")
+        fo.write(secCode + "-" + secName + "\n")
         fo.close()
         return True
 
