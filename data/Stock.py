da1367df418efc6aa5e6665da6857410321d0d5e
宁铭杰 <bream_n@163.com>
2018-10-31 18:07:39 +0800

    暂存

--- data/Stock.py
+++ data/Stock.py
@@ -18,16 +18,31 @@ class Stock:
         self._startDate = time.strftime('%Y%m%d',time.localtime(time.time()-15*24*3600))
         self._endDate = time.strftime('%Y%m%d',time.localtime(time.time()))
 
+
+    """
+    获取交易列表
+    """
     def getTradeCal(self):
         ts.set_token('9caf3d505f4f4b5cefd16f25c533e1cae081773442c216888678ddee')
         pro = ts.pro_api()
         data = pro.trade_cal(exchange_id='', start_date=self._startDate, end_date=self._endDate)
         data.reset_index(inplace=True)
+        data = pd.DataFrame(data, columns=['is_open', 'cal_date'])
         print data.to_dict('records')
 
-        sk = ts.get_hist_data('000713', '2018-10-23', '2018-10-31')
+    """
+    判断是否开盘
+    secCode ：股票代码
+    _date ：请求日期
+    """
+    def isOpen(self, secCode, _date):
+        sk = ts.get_hist_data(secCode, _date, _date)
         sk.reset_index(inplace=True)
-        print sk.to_dict('records')
+        if len(sk.to_dict('records')):
+            return True
+
+        return False
+
 
 
 
