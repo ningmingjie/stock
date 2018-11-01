63b2322c21a4463b0c4944e423cc417b21afdd31
宁铭杰 <bream_n@163.com>
2018-11-01 15:03:27 +0800

    停复牌测试

--- data/Write.py
+++ data/Write.py
@@ -13,19 +13,17 @@ class Write:
 
     def __init__(self):
         self._path = "/data/share/loudou/stock/"
-        #ts.set_token('9caf3d505f4f4b5cefd16f25c533e1cae081773442c216888678ddee')
+        ts.set_token('9caf3d505f4f4b5cefd16f25c533e1cae081773442c216888678ddee')
 
     def getTodayAll(self):
-        data = ts.get_today_all()
-        #pro = ts.pro_api()
-        #data = pro.query('stock_basic', exchange_id='', list_status='L',
-                         #fields='ts_code,symbol,name,area,industry,list_date')
+        pro = ts.pro_api()
+        data = pro.query('stock_basic', exchange_id='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
         data.reset_index(inplace=True)
         dt = np.array(data)
         dts = dt.tolist()
 
         for st in dts:
-            self.write(st[1], st[2])
+            self.write(st[2], st[3])
         return True
 
     def write(self, secCode, secName):
