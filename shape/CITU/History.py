eefecb8ed147b008dfe95be47dea113af0be6f7e
宁铭杰 <bream_n@163.com>
2018-11-09 16:04:55 +0800

    暂存

--- shape/CITU/History.py
+++ shape/CITU/History.py
@@ -62,53 +62,8 @@ class History:
             else:
                 defeated = defeated+1
                 is_succee = 10
+            print data[i-1]['date']
 
-            morrowIncome = 0
-            morrowPrice = 0
-            castDate = '1970-01-01'
-            appearDate = data[i-1]['date']
-            highIncome = 0
-            highPrice = 0
-            highPosition = 0
-            winRate = 0
-            periods = period
-            totalIncome = 0
-            totalPrice = 0
-
-            stage = 300
-            if (i-period)<0:
-                stage = 200
-                periods = i
-
-            if i-1 != 0:
-                morrowIncome = (data[i - 2]['close'] - data[i - 1]['close']) / data[i - 1]['close']
-                morrowPrice = data[i - 2]['close']
-                castDate = data[i - 1]['date']
-                for j in range(2, periods + 2):
-                    income = (data[i - j]['high'] - data[i - 1]['close']) / data[i - 1]['close']
-                    if income > highIncome:
-                        castDate = data[i - j]['date']
-                        highIncome = income
-                        highPrice = data[i - j]['high']
-                        highPosition = j - 1
-
-                totalIncome = (data[i - (periods + 1)]['close'] - data[i - 1]['close']) / data[i - 1]['close']
-                totalPrice = data[i - (periods + 1)]['close']
-                winRate = succee / float((succee + defeated))
-
-            sql = """INSERT INTO shape (shape_key, sec_id, sec_code, sec_name, is_succee, appear_date, cast_date, join_price,morrow_income, morrow_price, high_income, \
-high_price, total_income, total_price, best_position, total_position, win_rate, stage, created_at, updated_at) VALUES ('%s', '%s', \
-'%s', '%s', '%d', '%s', '%s', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%d', '%d', '%f', '%d',  '%d', '%d')""" % ('CITU', self.secID, self.secCode, self.secName, \
-is_succee, appearDate, castDate, data[i-1]['close'],morrowIncome, morrowPrice, highIncome, highPrice, totalIncome, totalPrice, highPosition, periods, winRate, stage, int(time.time()), int(time.time()))
-            stock_db.insertData(sql)
-            id = stock_db.getLastId()
-
-            if id > 0:
-                for k in range(0, 2):
-                    shapeDetail = """INSERT INTO shape_detail (shape_id, shape_date, shape_price, shape_income, created_at, updated_at) VALUES ('%d', '%s','%f', '%f', '%d', '%d')""" % ( \
-                        id, data[i-k]['date'], data[i-k]['close'], data[i-k]['p_change']/100, int(time.time()), int(time.time()))
-                    stock_db.insertData(shapeDetail)
-        return True
 
 class Stock:
     def getStockAll(self):
