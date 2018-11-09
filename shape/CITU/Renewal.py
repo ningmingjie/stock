ef862b629ffa893b29abda9e170e8c7eaffb04a2
宁铭杰 <bream_n@163.com>
2018-11-09 16:53:10 +0800

    刺透测试

--- shape/CITU/Renewal.py
+++ shape/CITU/Renewal.py
@@ -38,6 +38,7 @@ class Renewal:
 
         for i in range(0, len(data)):
             cal = Stock.getStockCal(data[i]['sec_code'], Date.getDateAmend(self._date), Date.getDateAmend(self._date))
+            print cal
             if len(cal) <= 0:
                 continue
             morrowIncome = data[i]['morrow_income']
