bf0486528fc0722e48c412b64194ba73f1c187a9
宁铭杰 <bream_n@163.com>
2018-11-09 16:51:44 +0800

    刺透测试

--- shape/CITU/Renewal.py
+++ shape/CITU/Renewal.py
@@ -20,7 +20,8 @@ class Renewal:
         self.secCode = secCode
         self.secName = secName
         self.secID = secID
-        self._date = time.strftime('%Y%m%d', time.localtime(time.time()))
+        #self._date = time.strftime('%Y%m%d', time.localtime(time.time()))
+        self._date = '20181031'
 
     """
     获取形态运行中的股票
