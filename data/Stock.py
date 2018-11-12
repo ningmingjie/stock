598790178b44745c77166a5da91534ecc674e77a
宁铭杰 <bream_n@163.com>
2018-11-12 17:00:33 +0800

    获取上一个交易日

--- data/Stock.py
+++ data/Stock.py
@@ -123,7 +123,8 @@ class Stock:
 
         pro = ts.pro_api()
         data = pro.query('trade_cal', start_date=Date.getDiffDate(_date, 15), end_date=Date.getDateFormat(_date, '%Y-%m-%d', '%Y%m%d'))
-        print data[data.is_open==1].to_dict('records')
+        dict = data[data.is_open==1].to_dict('records')
+        print dict[len(dict)-1]
 
     @staticmethod
     def getStockInfo(secID):
