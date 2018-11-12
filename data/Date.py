6b9f6454f301e1ee1fcbf21193ee1a53a1de43f4
宁铭杰 <bream_n@163.com>
2018-11-12 16:31:40 +0800

    获取上一个交易日

--- data/Date.py
+++ data/Date.py
@@ -41,8 +41,11 @@ class Date:
     @staticmethod
     def getDiffDate(_date, _number, _type = 'redu', expectFormat = '%Y%m%d', format = '%Y-%m-%d'):
         if _type == 'redu':
-            return time.strftime(expectFormat,
+            print 12
+            tk =  time.strftime(expectFormat,
                                  time.localtime(time.mktime(time.strptime(_date, format)) - _number * 24 * 3600))
+            print 23
+            return tk
         else:
             return time.strftime(expectFormat,
                                  time.localtime(time.mktime(time.strptime(_date, format)) + _number * 24 * 3600))
