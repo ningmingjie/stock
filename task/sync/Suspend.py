9a01e865bed05c78298c9bd4322042236a7bbfc5
宁铭杰 <bream_n@163.com>
2018-11-13 15:46:37 +0800

    测试

--- task/sync/Suspend.py
+++ task/sync/Suspend.py
@@ -56,8 +56,10 @@ class Suspend:
                     upSql = """UPDATE suspend SET suspend_type = %d, resum_date = '%s'  WHERE sec_code = '%s' AND suspend_date = '%s'""" % (20, val[8], stock['sec_code'], val[7])
                 stock_db.update(upSql)
                 continue
-            sql = """INSERT INTO suspend (sec_id, sec_code, sec_name, suspend_type, suspend_date, suspend_reason, created_at, updated_at) VALUES ('%s', '%s', '%s', '%d', '%s', \
-'%s', '%d', '%d')""" % (stock['sec_id'], stock['sec_code'], stock['sec_name'], 10, Date.getDate(val[2]), val[5],int(time.time()), int(time.time()))
+            if Date.getTimestamp(Date.getDate(val[3]), '%Y-%m-%d') < Date.getTimestamp(self._date, '%Y-%m-%d'):
+                resumDate = val[8]
+            sql = """INSERT INTO suspend (sec_id, sec_code, sec_name, suspend_type, resum_date, suspend_date, suspend_reason, created_at, updated_at) VALUES ('%s', '%s', '%s', '%d', '%s', \
+'%s', '%d', '%d')""" % (stock['sec_id'], stock['sec_code'], stock['sec_name'], 10, resumDate,Date.getDate(val[2]), val[5],int(time.time()), int(time.time()))
             stock_db.insertData(sql)
         return True
 
