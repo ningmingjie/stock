518884e57a6ca38b483bb05aa527bf6a2859a168
宁铭杰 <bream_n@163.com>
2018-11-13 15:37:45 +0800

    测试

--- task/sync/Suspend.py
+++ task/sync/Suspend.py
@@ -45,7 +45,7 @@ class Suspend:
             stock = Stock.getCodeStockInfo(val[0])
             if stock == None:
                 continue
-            reSql = """SELECT * FROM suspend WHERE sec_code = '%s' AND resum_date = '%s'""" % (stock['sec_code'], '1970-01-01')
+            reSql = """SELECT * FROM suspend WHERE sec_code = '%s' AND resum_date = '%s' AND suspend_type = %d""" % (stock['sec_code'], '1970-01-01', 20)
             query = stock_db.fetch_one(reSql)
             if query != None:
                 if Date.getTimestamp(Date.getDate(val[3]), '%Y-%m-%d') == Date.getTimestamp(self._date, '%Y-%m-%d'):
@@ -67,7 +67,7 @@ for i in range(0, 12):
     suspend = Suspend(_date[i])
     #_date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
     _dates = _date[i]
-    sql = """UPDATE suspend SET resum_date = '%s' WHERE suspend_type = %d """ % ('1970-01-01', 10)
+    sql = """UPDATE suspend SET resum_date = '%s', suspend_type = %d WHERE suspend_type = %d """ % ('1970-01-01', 20, 10)
     stock_db.update(sql)
     for i in range(1, 5):
         res = suspend.getData(i)
